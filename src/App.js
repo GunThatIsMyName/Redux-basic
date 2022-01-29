534c3053430f0dfe6e696ef7a71660ff6c8d9135
Junhyun <mnmthatisus@gmail.com>
2022-01-29 09:33:01 +0900

    state change

--- src/App.js
+++ src/App.js
@@ -10,20 +10,55 @@ import cartItems from "./cart-items";
 import {createStore} from "redux"
 
 const initailStore = {
-  count:0
+  count:0,
+  name:"Cono"
 }
 
 const reducer=(state,action)=>{
   console.log({state,action});
+  if(action.type === "DECREASE"){
+    return{
+      ...state,count:state.count-1
+    }
+  }
+
+  if(action.type === "INCREASE"){
+    return{
+      ...state,count:state.count+1
+    }
+  }
+
+  if(action.type === "RESET"){
+    return{
+      ...state,count:0
+    }
+  }
+
+  if(action.type === "NAME_CHANGE"){
+    return{
+      ...state,count:100,name:"MINJI"
+    }
+  }
+
+
   // return state
   return state;
 }
 
 const store = createStore(reducer,initailStore);
+
+
+store.dispatch({type:"DECREASE"})
+store.dispatch({type:"INCREASE"})
+store.dispatch({type:"RESET"})
+
+store.dispatch({type:"NAME_CHANGE"})
+
 // 리덕스 파트 ---------------------------------
 
 function App() {
 
+  console.log(store.getState())
   // cart setup
 
   return (
